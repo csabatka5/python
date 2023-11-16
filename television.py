e1750fea77f0b53c242518eaaa2b38db6586eff2
csabatka5 <csabatka@unomaha.edu>
2023-11-16 10:35:03 -0600

    Update television.py

--- television.py
+++ television.py
@@ -11,18 +11,36 @@ class Television:
         self.__channel = Television.MIN_CHANNEL
 
     def power(self):
+        '''
+        This function turns the tv on and off by changing
+        the value of the status variable.
+        '''
         self.__status = not self.__status
 
     def mute(self):
+        '''
+        This function mutes and unmutes the tv when it is
+        on by changing the value of the muted variable.
+        '''
         if self.__status:
             self.__muted = not self.__muted
 
     def channel_up(self):
+        '''
+        This function increases the tv channel value when the
+        tv is on. If the tv is at max channel it will go to
+        the minimum channel.
+        '''
         if self.__channel < Television.MAX_CHANNEL:
             self.__channel += 1
         else:
             self.__channel = Television.MIN_CHANNEL
     def channel_down(self):
+        '''
+        This function decreases the tv channel value when the
+        tv is on. If the tv is at min channel it will go to
+        the maximum channel.
+        '''
         if self.__status:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
@@ -30,12 +48,22 @@ class Television:
                 self.__channel = Television.MAX_CHANNEL
 
     def volume_up(self):
+        '''
+        This function increases the tv volume value when the
+        tv is on. If the tv is at max volume it will remain at
+        the max volume.
+        '''
         if self.__status:
             self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
     def volume_down(self):
+        '''
+        This function decreases the tv volume value when the
+        tv is on. If the tv is at min volume it will remain at
+        the min volume.
+        '''
         if self.__status:
             self.__muted = False
             if self.__volume > Television.MIN_VOLUME:
