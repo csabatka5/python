58eedb86ac33535987e70f340bb7d0f19b94a260
csabatka5 <csabatka@unomaha.edu>
2023-11-17 13:06:27 -0600

    Update television.py

--- television.py
+++ television.py
@@ -4,20 +4,20 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
         '''
         This function turns the tv on and off by changing
         the value of the status variable.
         '''
         self.__status = not self.__status
 
-    def mute(self):
+    def mute(self) -> None:
         '''
         This function mutes and unmutes the tv when it is
         on by changing the value of the muted variable.
@@ -25,7 +25,7 @@ class Television:
         if self.__status:
             self.__muted = not self.__muted
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         '''
         This function increases the tv channel value when the
         tv is on. If the tv is at max channel it will go to
@@ -36,7 +36,7 @@ class Television:
                 self.__channel += 1
             else:
                 self.__channel = Television.MIN_CHANNEL
-    def channel_down(self):
+    def channel_down(self) -> None:
         '''
         This function decreases the tv channel value when the
         tv is on. If the tv is at min channel it will go to
@@ -48,7 +48,7 @@ class Television:
             else:
                 self.__channel = Television.MAX_CHANNEL
 
-    def volume_up(self):
+    def volume_up(self) -> None:
         '''
         This function increases the tv volume value when the
         tv is on. If the tv is at max volume it will remain at
@@ -59,7 +59,7 @@ class Television:
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         '''
         This function decreases the tv volume value when the
         tv is on. If the tv is at min volume it will remain at
@@ -70,7 +70,7 @@ class Television:
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
         if self.__muted:
             return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}'
         else:
